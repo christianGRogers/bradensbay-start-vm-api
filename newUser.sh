eba82a55ad25f1474d4a40da7ac12c06672e85d8
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2024-11-21 18:03:52 -0500

    Update newUser.sh

--- newUser.sh
+++ newUser.sh
@@ -21,10 +21,10 @@ fi
 # Wait for the VM to start (if it dose not start in 20 sec ur cooooked)
 sleep 40
 
-# Install Apache2 on the LXD VM
+# Install Apache2 on the LXD VM export DEBIAN_FRONTEND=noninteractive; <============fuck this
 echo "Installing Apache2 on the LXD VM: $USER_ID"
-lxc exec $USER_ID -- bash -c "export DEBIAN_FRONTEND=noninteractive; export http_proxy=http://10.0.0.11:3128; apt-get update"
-lxc exec $USER_ID -- bash -c "export DEBIAN_FRONTEND=noninteractive; export http_proxy=http://10.0.0.11:3128; apt-get install -y apache2"
+lxc exec $USER_ID -- bash -c "export http_proxy=http://10.0.0.11:3128; apt-get update"
+lxc exec $USER_ID -- bash -c "export http_proxy=http://10.0.0.11:3128; apt-get install -y apache2"
 
 
 # Create the folder named by the argument in /var/www/html and move index.html into that folder
